70d5c62d54cfff976207fcf1d42f1d36c675f69f
Khoma Olya <52779547+khomavolha@users.noreply.github.com>
2020-03-20 22:42:50 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -3,9 +3,37 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+//Проверка на парность скобок
+function bracketsCouple(str){
+let all = ['-', '/', '+', '*'];
+for(am = 0; am<all.length; am++){
+if(str.includes(all[am])) {
+  str = str.replace(/[0-9]/g, '');
+  str = str.replace(all[am], '');
+am=-1;}}
+const br = [ '()' ];
+for (let i = 0; i < br.length; i++) {
+ if (str.includes(br[i])) {
+   str = str.replace(br[i], "");
+ i=-1;}}
+for (let i = 0; i < br.length; i++) {
+if(str.length !== 0) {throw new Error('ExpressionError: Brackets must be paired')}else{return true}}}
+
+
+function exit(one){
+  return new Function('return ' + one)();
 }
+
+
+//Деление на ноль и вывод всего
+function expressionCalculator (expr) {
+  expr = expr.split(' ').filter(el => el != '').join('');
+let noZero = expr.trim().includes('/0');
+if (noZero){throw new Error('TypeError: Division by zero.')}
+if(bracketsCouple(expr))
+  return exit(expr);
+}
+
 
 module.exports = {
     expressionCalculator
